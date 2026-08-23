11c105b94a02fc0c290f7a6ed332f1f23195a786
VitaliyBay <vitaliygbbay@gmail.com>
2026-08-23 02:07:36 +0200

    Add sendMCP and readMCP methods to ESP32-MSP's main file

--- ESP32-MSP/ESP32-MSP/src/main.cpp
+++ ESP32-MSP/ESP32-MSP/src/main.cpp
@@ -1,12 +1,82 @@
 #include <Arduino.h>
 
+#define FC_RX_PIN 16
+#define FC_TX_PIN 17
+#define MSP_STATUS 101
+#define MSP_ANALOG 110
+
+HardwareSerial FCSerial(2);
+
+void sendMCP(uint8_t command) {
+  uint8_t checksum = command;
+
+  // MCP v1 package:
+  // $ M < data_length command checksum
+
+  FCSerial.write('$');
+  FCSerial.write('M');
+  FCSerial.write('<');
+
+  // Payload length = 0
+  FCSerial.write((uint8_t)0);
+
+  // Command
+  FCSerial.write(command);
+
+  // Checksum
+  FCSerial.write(checksum);
+}
+
+bool readMSP(uint8_t &command, uint8_t *payload, uint8_t &payloadSize) {
+  // Find '$'
+  if (FCSerial.read() != '$') {
+    return false;
+  }
+
+  if (FCSerial.read() != 'M') {
+    return false;
+  }
+
+  if (FCSerial.read() != '>') {
+    return false;
+  }
+
+  uint8_t size = FCSerial.read();
+  uint8_t cmd = FCSerial.read();
+
+  uint8_t checkSum = size ^ cmd;
+
+  for (uint8_t i = 0; i < size; i++) {
+    payload[i] = FCSerial.read();
+    checkSum ^= payload[i];
+  }
+
+  uint8_t receivedChecksum = FCSerial.read();
+
+  if (checkSum != receivedChecksum) {
+    Serial.println("Checksum error!");
+    return false;
+  }
+
+  command = cmd;
+  payloadSize = size;
+
+  return true;
+}
+
 void setup() {
   Serial.begin(115200);
+
+  FCSerial.begin(115200, SERIAL_8N1, FC_RX_PIN, FC_TX_PIN);
+
   Serial.println("ESP32 started!");
+  Serial.println("FC UART initialized");
 }
 
 void loop() {
-  Serial.println("Hello from ESP32");
+  while(FCSerial.available()) {
+    uint8_t byte = FCSerial.read();
 
-  delay(1000);
+    Serial.printf("%02X ", byte);
+  }
 }
